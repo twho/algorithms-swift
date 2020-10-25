8221e17673e1cbb612f02edde25f79a4e305ec3b
Michael Ho <michael.ho01@verizonmedia.com>
2020-10-24 18:17:07 -0700

    Setup Swift project

---
+++ AlgorithmsSwift/AlgorithmsSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AlgorithmsSwift
+//
+//  Created by Michael Ho on 10/24/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ AlgorithmsSwift/AlgorithmsSwiftTests/Test1.swift
@@ -0,0 +1,16 @@
+//
+//  Test1.swift
+//  AlgorithmsSwiftTests
+//
+//  Created by Michael Ho on 10/24/20.
+//
+
+import XCTest
+
+class Test1: XCTestCase {
+
+    func testA() {
+        
+    }
+
+}
